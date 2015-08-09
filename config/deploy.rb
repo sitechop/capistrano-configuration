b4900cf66c25337a3d3d90cf27aacd879248229d
Robby Klein <itsrobbyk@gmail.com>
2015-08-09 09:58:41 -0700

    Initial Commit

---
+++ config/deploy.rb
@@ -0,0 +1,22 @@
+lock '3.4.0'
+
+set :application, 'sitechop'
+set :repo_url, 'git@github.com:username/repo.git'
+
+set :deploy_to, '/var/www/sitechop.com'
+
+set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,2 @@
+set :stage, :production
+server '111.111.111.111', user: 'deploy', roles: %w{web app}
